a5a9904ecb540d02ee449f88cdb73e8669e6385e
LouisLiu <louis19820503@gmail.com>
2016-11-07 16:24:46 +0800

    First Example

---
+++ UnitTestExample/Examples/Math.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Examples
+{
+    public class Math
+    {
+        public int Max(int val1, int val2)
+        {
+            return (val1 >= val2) ? val1 : val2;
+        }
+    }
+}
---
+++ UnitTestExampleTests/Examples/MathTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestExample.Examples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Examples.Tests
+{
+    [TestClass()]
+    public class MathTests
+    {
+        [TestMethod()]
+        public void MaxTest()
+        {
+            //arrange
+            var target = new Math();
+            var input1 = 1;
+            var input2 = 2;
+            var expected = 2;
+            //act
+            var actual = target.Max(input1, input2);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxTest2()
+        {
+            //arrange
+            var target = new Math();
+            var input1 = 1;
+            var input2 = 2;
+            var expected = 3;
+            //act
+            var actual = target.Max(input1, input2);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
